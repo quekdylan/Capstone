5bcb1f3e87961e986affc9b210e107456f27d4c4
Sng12345jm <60732810+Sng12345jm@users.noreply.github.com>
2021-02-24 01:26:12 +0800

    Update Servo_Wagon.ino

--- Jun Min/Servo_Wagon.ino
+++ Jun Min/Servo_Wagon.ino
@@ -6,20 +6,17 @@ Emakefun_MotorDriver mMotorDriver = Emakefun_MotorDriver(0x60, MOTOR_DRIVER_BOAR
 Emakefun_Servo *servoLeft = mMotorDriver.getServo(1); //port 1
 Emakefun_Servo *servoRight = mMotorDriver.getServo(2); //port 2
 
-// Servo servoRight;
 const int TrigPin = A2;
 const int EchoPin = A3;
 float distance;
+// starting angle for left and right servo
 int langle0 = 70;
 int langle1 = 0;
 int rangle0 = 60;
 int rangle1 = 130; 
-String per = "";
 
 void setup() {
   // put your setup code here, to run once:
-  //servoLeft.attach(1); // pin, min, max
-  // servoRight.attach(2);
   Serial.begin(9600);
   pinMode(TrigPin, OUTPUT);
   pinMode(EchoPin, INPUT);
@@ -29,7 +26,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(TrigPin, LOW);
+  digitalWrite(TrigPin, LOW); 
   delayMicroseconds(2);
   digitalWrite(TrigPin, HIGH);
   delayMicroseconds(10);
@@ -37,34 +34,33 @@ void loop() {
   distance = pulseIn(EchoPin, HIGH) / 58.00;
   Serial.print("distance is :");
   Serial.print(distance);
-  Serial.print("cm");
-  Serial.print("\n");
+  Serial.print("cm"); 
+  Serial.print("\n"); // (till here) to see the distance between wagon's content and ultrasonic sensor
   delay(500);
-  servoLeft->writeServo(langle0);
+  servoLeft->writeServo(langle0); //setting the servo original position
   servoRight->writeServo(rangle0);
   delay(500);
 
 
-  if (distance < 4) 
+  if (distance < 5) 
   {
     int r = 0;
     int i = 200;
-    tone(A0, i, 500);
+    tone(A0, i, 500); // to signify Arduino communicating with ROS
     delay(5000);
-    tone(A0, i, 500);
+    tone(A0, i, 500); // to signify Arduino receiving command from ROS
     delay(5000);
-    for (int l = 70; l >= 0; l--){
+    for (int l = 70; l >= 0; l--) // Lifting the wagon
+    { 
       r = 130-l;
-      //r = rangle0;
       servoLeft->writeServo(l);
       servoRight->writeServo(r);
       delay(30);
     }
     delay(2000);
-    for (int l = 0; l <= 70; l++)
+    for (int l = 0; l <= 70; l++) // Grounding the wagon
     {
       r = 130-l;
-      //r = rangle1;
       servoLeft->writeServo(l);
       servoRight->writeServo(r);
       delay(50);
